cfd664ba09f41bfc6be6e5929c58e848ded4f636
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2025-09-18 15:32:30 +0800

    Update GMP to 6.3.0 and fix GCC 15 build

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,12 +1,14 @@
 package=gmp
-$(package)_version=6.2.1
+$(package)_version=6.3.0
 $(package)_download_path=https://gmplib.org/download/$(package)/
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
-$(package)_patches=arm64-avoid-x18.patch
+$(package)_sha256_hash=a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898
+$(package)_patches=0001-Complete-function-prototype-in-acinclude.m4-for-C23-.patch
 
+# arm64-avoid-x18.patch is already included in GMP 6.3.0 (https://github.com/gmp-mirror/gmp/commit/1aaf0e4a34b25e73f5505151ab12397031318985)
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/arm64-avoid-x18.patch
+  patch -p1 < $($(package)_patch_dir)/0001-Complete-function-prototype-in-acinclude.m4-for-C23-.patch && \
+  autoconf
 endef
 
 define $(package)_set_vars
--- src/zmq/zmqabstractnotifier.h
+++ src/zmq/zmqabstractnotifier.h
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include <string>
+#include <cstdint>
 
 class CBlockIndex;
 class CTransaction;
